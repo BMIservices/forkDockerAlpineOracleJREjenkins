9a00cd3cdd9f4b40c4ccdbb343768e572617f226
Stuart Wong <cgs.wong@gmail.com>
2015-05-27 16:33:40 -0500

    Fixed command line option issue.

--- jenkins.sh
+++ jenkins.sh
@@ -5,8 +5,8 @@ set -eo pipefail
 JENKINS_HOME=/opt/jenkins
 PLUGINS_ENDPOINT=${PLUGINS_ENDPOINT:-"http://updates.jenkins-ci.org/latest/"}
 JENKINS_PLUGINS=/var/lib/jenkins/plugins
-#JENKINS_USER=jenkins
-#JENKINS_GROUP=jenkins
+JENKINS_USER=jenkins
+JENKINS_GROUP=jenkins
 
 # Install plugins
 mkdir -p $JENKINS_PLUGINS
@@ -14,12 +14,11 @@ for plugins in credentials ssh-credentials ssh-agent ssh-slaves git-client git g
   curl --silent --location --insecure $PLUGINS_ENDPOINT/${plugins}.hpi --output $JENKINS_PLUGINS/${plugins}.hpi
 done
 
-# chown -R $JENKINS_USER:$JENKINS_GROUP $JENKINS_PLUGINS
+chown -R $JENKINS_USER:$JENKINS_GROUP $JENKINS_PLUGINS
 
 # if `docker run` first argument start with `--` the user is passing Jenkins launcher arguments
-if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  exec java -jar ${JENKINS_HOME}/jenkins.war "$@"
+if [[ $# -ge 1 ]] || [[ "$1" == "--"* ]]; then
+  java -jar ${JENKINS_HOME}/jenkins.war "$@"
+else
+  exec "$@"
 fi
-
-# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
-exec "$@"
